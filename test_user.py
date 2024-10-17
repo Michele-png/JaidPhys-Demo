69e2fbec0683f765e3f8fe0d71ca6c157c6ae0c3
Michele Scalzotto <scalzottomichele@gmail.com>
2024-10-17 20:43:06 +0200

    commit

--- test_user.py
+++ test_user.py
@@ -27,19 +27,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-
-
-
-    webrtc_ctx = webrtc_streamer(
-        key="posture-detection",
-        mode=WebRtcMode.SENDRECV,
-        rtc_configuration={
-            "iceServers": get_ice_servers(),
-            "iceTransportPolicy": "relay",
-        },
-        video_processor_factory=VideoTransformer,
-        # video_frame_callback=video_frame_callback,
-        media_stream_constraints={"video": True, "audio": False},
-        async_processing=True,
-    )
