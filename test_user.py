bf45f4cb0759c3dc374ac78bc56c9c8a363c2d9f
Michele Scalzotto <63316157+Michele-png@users.noreply.github.com>
2024-10-18 11:43:12 +0200

    Update test_user.py

--- test_user.py
+++ test_user.py
@@ -3,32 +3,24 @@ from streamlit_webrtc import WebRtcMode, VideoTransformerBase, webrtc_streamer, 
 from sample_utils.turn import get_ice_servers
 import av
 
-# class VideoTransformer(VideoTransformerBase):
-#     def transform(self, frame):
-#         # Here you can process the frame if needed
-#         return frame
+class VideoProcessor:
+    def recv(self, frame):
+        img = frame.to_ndarray(format="bgr24")
+        return av.VideoFrame.from_ndarray(img, format="bgr24")
 
-def video_frame_callback(frame: av.VideoFrame) -> av.VideoFrame:
-    image = frame.to_ndarray(format="bgr24")
-    return av.VideoFrame.from_ndarray(image, format="bgr24")
+RTC_CONFIGURATION = RTCConfiguration(
+    {"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]}
+    
+st.title("Camera Stream with Streamlit WebRTC")
+st.write("This app captures the camera image stream and displays it on the interface.")
 
-def main():
-    st.title("Camera Stream with Streamlit WebRTC")
-    st.write("This app captures the camera image stream and displays it on the interface.")
+# Set up the WebRTC streamer with STUN server
+webrtc_ctx = webrtc_streamer(
+    key="posture-detection",
+    mode=WebRtcMode.SENDRECV,
+    rtc_configuration = RTC_CONFIGURATION,
+    media_stream_constraints={"video": True, "audio": False},
+    video_processor_factory=VideoProcessor,
+    async_processing=True,
+)
 
-    # Set up the WebRTC streamer with STUN server
-    webrtc_ctx = webrtc_streamer(
-        key="posture-detection",
-        mode=WebRtcMode.SENDRECV,
-        rtc_configuration = {
-            "iceServers": get_ice_servers(),
-            "iceTransportPolicy": "relay",
-        },
-        # video_processor_factory=VideoTransformer,
-        # video_frame_callback=video_frame_callback,
-        media_stream_constraints={"video": True, "audio": False},
-        async_processing=True,
-    )
-
-if __name__ == "__main__":
-    main()
