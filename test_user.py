39af6c41701ad54a2aca6412aa7ec9bedce51b93
Michele Scalzotto <63316157+Michele-png@users.noreply.github.com>
2024-10-18 11:33:24 +0200

    Update test_user.py

--- test_user.py
+++ test_user.py
@@ -20,9 +20,10 @@ def main():
     webrtc_ctx = webrtc_streamer(
         key="posture-detection",
         mode=WebRtcMode.SENDRECV,
-        rtc_configuration = RTCConfiguration(
-            {"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]}
-        ),
+        rtc_configuration = {
+            "iceServers": get_ice_servers(),
+            "iceTransportPolicy": "relay",
+        },
         # video_processor_factory=VideoTransformer,
         # video_frame_callback=video_frame_callback,
         media_stream_constraints={"video": True, "audio": False},
