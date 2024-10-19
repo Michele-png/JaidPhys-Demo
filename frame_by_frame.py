816b150663b5058992d06a805358fe2f306c63d7
Michele Scalzotto <63316157+Michele-png@users.noreply.github.com>
2024-10-19 17:51:21 +0200

    Update frame_by_frame.py

--- frame_by_frame.py
+++ frame_by_frame.py
@@ -30,7 +30,7 @@ if uploaded_file is not None:
     st.write(f"fps: {fps}")
     delay = 5 / fps if fps > 0 else 0.03  # Adjust delay based on video fps
     st.write(f"delay: {delay}")
-    n_skipped = 3
+    n_skipped = 5
     total_frames = int(vf.get(cv.CAP_PROP_FRAME_COUNT))
     st.write(f"Total frames in video: {total_frames}")
 
